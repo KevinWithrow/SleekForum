65d964818542c10bc2103fab5327ee60cea40453
justinbaker10 <justin-baker@live.com>
2020-05-18 20:09:07 -0500

    setting up handlebars

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express')
+
+const app = express()
+
+//serve files out of the public directory
+app.use(express.static('public'))
+
+const port = 9999
+
+app.set('view engine', 'hbs')
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.listen(port, () => {
+    console.log('express is listening on port ' + port)
+})
